58ed954085f058f933986325878af4d6bffe28bf
Rajesh M <yem.rajesh@yahoo.co.in>
2018-06-06 09:13:21 -0400

    Test
    Signed-off-by: Rajesh M <yem.rajesh@yahoo.co.in>

---
+++ SampleJenkinsProj/src/main/java/TestPackage/TestFile.java
@@ -0,0 +1,14 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class TestFile {
+
+	@Test
+	public static void test() {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test project");
+
+	}
+
+}
